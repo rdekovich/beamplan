841725c3971244d0f14a6f18bfd7c20a2004803b
Robert Dekovich <dekovich@umich.edu>
2020-10-10 13:47:17 -0700

    Added setup.py base file

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Setup package installation file for the beamplan package.
+
+Module can be invoked in two ways.  First, it can be
+called by invoking the interpreter directly.  This is
+done by the following:
+
+$ ./setup.py
+
+It can also be invoked via pip.  This can be done by the
+following:
+
+$ pip install -e .
+"""
+
+__author__ = "Robert Dekovich"
+__email__ = "dekovich@umich.edu"
+__status__ = "Development"
+
+from setuptools import setup
+
+setup(
+    name="beamplan",
+    version="0.0.0",
+    description="A command-line tool to determine Starlink beam planning.",
+    author=__author__,
+    author_email=__email__,
+    packages=["beamplan"],
+    include_package_data=True,
+    install_requires=[
+        "pycodestyle==2.5.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "beamplan = beamplan.__main__:main"
+        ]
+    }
+)
